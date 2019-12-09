45f704faf1c3a76333c1dda1488c5f47022ea599
Igor Evgrafov <igor.evgrafov@aurea.com>
2019-12-10 02:44:53 +0300

    Generate csv

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const rp = require('request-promise-any');
 const { Parser } = require('json2csv');
 const fs = require('fs');
 const Promise = require('bluebird');
+const csvToJson = require('csvtojson');
 
-async function test(address) {
+async function resolveAddress(address) {
   const url = `https://geocode-maps.yandex.ru/1.x/?apikey=${yandex.key}&geocode=${encodeURIComponent(address)}&format=json`;
   return rp({
     url,
@@ -14,78 +15,91 @@ async function test(address) {
   });
 }
 
+const LATITUDE = 'Широта';
+const LONGITUDE = 'Долгота';
+const DESCRIPTION1 = 'Описание';
+const DESCRIPTION2 = 'Подпись';
+const LINE_NO = 'Номер метки';
+
 const opts = {
-  fields: ['Широта', 'Долгота', 'Описание', 'Подпись', 'Номер метки'],
+  fields: [LATITUDE, LONGITUDE, DESCRIPTION1, DESCRIPTION2, LINE_NO],
   delimiter: ';'
 };
 const parser = new Parser(opts);
 
 Promise.resolve()
   .then(async() => {
-    const address = '121099, г.Москва, ул.Новый Арбат, д.23';
-    const description = 'Общество с ограниченной ответственностью "ЛИК-Лидер"';
 
-    function log(message) {
-      console.info(`${address} : ${message}`);
-    }
+    const addresses = await csvToJson().fromFile('addresses.csv');
+    let lineNumber = 1;
 
-    function toCsvRecord(response) {
-      if (!response) {
-        log('No response');
-        return;
+    const records = await Promise.mapSeries(addresses, async input => {
+      const address = input.address;
+      const description = input.description;
+
+      function log(message) {
+        console.info(`${address} : ${message}`);
       }
 
-      const geo = response.GeoObjectCollection;
-      if (!geo) {
-        log('No GeoObjectCollection');
-        return;
-      }
-      const metaDataProperty = geo.metaDataProperty;
-      if (!metaDataProperty) {
-        log('No metaDataProperty');
-        return;
-      }
-      const geocoderResponseMetaData = metaDataProperty.GeocoderResponseMetaData;
-      if (!geocoderResponseMetaData) {
-        log('No GeocoderResponseMetaData');
-        return;
-      }
-      const found = Number(geocoderResponseMetaData.found);
-      if (!found || found < 1) {
-        log(`Not found ${found}`);
-        return;
-      }
-      const featureMember = geo.featureMember;
-      if (!featureMember || !featureMember[0]) {
-        log('No featureMember');
-        return;
-      }
-      const geoObject = featureMember[0].GeoObject;
-      if (!geoObject) {
-        log('No GeoObject');
-        return;
-      }
-      const point = geoObject.Point;
-      if (!point || !point.pos) {
-        log('No Point');
-        return;
-      }
-      const [longitude, latitude] = point.pos.split(' ');
+      function toCsvRecord(response) {
+        if (!response) {
+          log('No response');
+          return;
+        }
 
-      return  {
-        'Широта': Number(latitude),
-        'Долгота': Number(longitude),
-        'Описание': description,
-        'Подпись': description,
-        'Номер метки': 1,
-      };
-    }
+        const geo = response.GeoObjectCollection;
+        if (!geo) {
+          log('No GeoObjectCollection');
+          return;
+        }
+        const metaDataProperty = geo.metaDataProperty;
+        if (!metaDataProperty) {
+          log('No metaDataProperty');
+          return;
+        }
+        const geocoderResponseMetaData = metaDataProperty.GeocoderResponseMetaData;
+        if (!geocoderResponseMetaData) {
+          log('No GeocoderResponseMetaData');
+          return;
+        }
+        const found = Number(geocoderResponseMetaData.found);
+        if (!found || found < 1) {
+          log(`Not found ${found}`);
+          return;
+        }
+        const featureMember = geo.featureMember;
+        if (!featureMember || !featureMember[0]) {
+          log('No featureMember');
+          return;
+        }
+        const geoObject = featureMember[0].GeoObject;
+        if (!geoObject) {
+          log('No GeoObject');
+          return;
+        }
+        const point = geoObject.Point;
+        if (!point || !point.pos) {
+          log('No Point');
+          return;
+        }
+        const [longitude, latitude] = point.pos.split(' ');
 
-    const response =  require('./test'); //await test("121099, г.Москва, ул.Новый Арбат, д.23");
+        const result = {};
+        result[LATITUDE] = Number(latitude);
+        result[LONGITUDE] = Number(longitude);
+        result[DESCRIPTION1] = description;
+        result[DESCRIPTION2] = description;
+        result[LINE_NO] = lineNumber++;
 
-    const record = toCsvRecord(response);
-    const csv = parser.parse([record]);
+        return  result;
+      }
 
+      log(`Processing ${lineNumber} ${address}`);
+      const response = await resolveAddress(address);
+      return toCsvRecord(response);
+    }).filter(it => !!it);
+
+    const csv = parser.parse(records);
     const file = process.argv[2];
     if (file) {
       fs.writeFileSync(file, csv)
